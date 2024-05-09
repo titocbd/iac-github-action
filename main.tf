7b6786219479546139a959f98178d6fc4de5f0cd
Mahbub Tito <tito@datasoft-bd.com>
2024-05-09 17:46:14 +0600

    s3 bucket implement

--- main.tf
+++ main.tf
@@ -1,48 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.32.0"
-    }
-  }
+resource "aws_ssm_parameter" "foo" {
+  name  = "foo"
+  type  = "String"
+  value = "barr"
 }
- 
-# Create VPC
-resource "aws_vpc" "tf_vpc" {
-  cidr_block = "10.0.0.0/16"
-  #enable_dns_support = true
-  #enable_dns_hostnames = true
-
-  tags = {
-    Name = "iac-vpc"
-  }
-}
-
-# Create public subnet
-resource "aws_subnet" "tf_public_subnets" { 
- vpc_id     = aws_vpc.tf_vpc.id
- cidr_block = "10.10.0.0/24"
-  # Data Center of this subnet.
-  availability_zone = "us-east-1a"
-  
-  # Enabling automatic public IP assignment on instance launch!
-  map_public_ip_on_launch = true
-
- tags = {
-   Name = "TF Public Subnet1"
- }
-}
- 
-# Create private subnet
-resource "aws_subnet" "tf_private_subnets" { 
- vpc_id     = aws_vpc.tf_vpc.id
- cidr_block = "10.11.0.0/24"
-  # Data Center of this subnet.
-  availability_zone = "us-east-1a"
- 
- tags = {
-   Name = "TF Private Subnet1"
- }
-}
- 
- 
